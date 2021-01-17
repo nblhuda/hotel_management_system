2df68055ef42bdfe46006af5cdf99ba3369c8af1
nblhuda <2018296332@isiswa.uitm.edu.my>
2021-01-17 13:13:39 +0800

    first commit

---
+++ about.php
@@ -0,0 +1,37 @@
+ <!-- Masthead-->
+        <header class="masthead">
+            <div class="container h-100">
+                <div class="row h-100 align-items-center justify-content-center text-center">
+                    <div class="col-lg-10 align-self-end mb-4" style="background: #0000002e;">
+                    	 <h1 class="text-uppercase text-white font-weight-bold">About Us</h1>
+                        <hr class="divider my-4" />
+                    </div>
+                    
+                </div>
+            </div>
+        </header>
+
+    <section class="page-section">
+        <div class="container">
+    <?php echo html_entity_decode($_SESSION['setting_about_content']) ?>   
+
+            
+        </div>
+        <div style="color: #E94256;border-bottom-style:solid;border-width:1px;width:60%;margin: auto;"></div>
+
+                <table>
+                <td style="padding:50px;">
+                    <iframe width="800" height="400" src="https://www.youtube.com/embed/kph5wOQzSEY"></iframe>
+                </td>
+                <td style="padding-right:50px;padding-top:50px;">
+                <h1 style="color: #E94256;">Safe & Sound at #MYRendahTecc #RendahTeccCares</h1><br>
+                <h3 style="color: #E94256;">Our Health & Safety Measures during Covid-19</h3><br>
+                   <div style="line-height: 1.5;">At RendahTecc Hotel Group, we take pride in offering you the finest family resort and vacationing experience. 
+                   In our effort to ensure that every aspect of your Lexis Experience remain perfectly intact despite the New Normal, we have put in place a set 
+                   of cleaning and safety measures that effectively safeguards you and your loved ones from check-in to check-out. The best of times begins with 
+                   a peace of mind, that’s why from the moment you walk through our doors, feel free to leave all your worries and concerns behind and allow yourself 
+                   a well-deserved moment of respite.</div>   
+                </td>
+                </table>
+        </div>
+        </section>
