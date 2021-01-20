d67de93fee4545d67aa1a43762f5d31dfad49b0c
nblhuda <2018296332@isiswa.uitm.edu.my>
2021-01-20 15:44:15 +0800

    display the facilities if status =0, if status =1 it will not display at index page

---
+++ facilities.php
@@ -0,0 +1,38 @@
+<!-- Masthead-->
+<header class="masthead">
+    <div class="container h-100">
+        <div class="row h-100 align-items-center justify-content-center text-center">
+            <div class="col-lg-10 align-self-end mb-4" style="background: #0000002e;">
+                <h1 class="text-uppercase text-white font-weight-bold">Facilities</h1>
+                <hr class="divider my-4" />
+            </div>
+        </div>
+    </div>
+</header>
+
+<section class="page-section"></section>
+	<div id="portfolio">
+            <div class="container-fluid p-0">
+                <div class="row no-gutters">
+                	<?php 
+					include'admin/db_connect.php';
+                    // select data from room facilities table
+                    // check the condition, it will display if status = 0
+                	$qry = $conn->query("SELECT * FROM  facilities where status = 0 ");
+                    while($row = $qry->fetch_assoc()):
+                    
+                    
+                	?>
+                    
+                    <div class="col-lg-4 col-sm-6">
+                        <a class="portfolio-box" href="#">
+                            <img class="img-fluid" src="assets/img/<?php echo $row['cover_img'] ?>" alt="" />
+                            <div class="portfolio-box-caption">
+                                <div class="project-name"><?php echo $row['name'] ?></div>
+                            </div>
+                        </a>
+                    </div>
+                	<?php endwhile; ?>
+                </div>
+            </div>
+        </div>
